f2488db94b1140087a709ff147a009ee9ab3a598
NaeemUllah6 <naeemullahkhokhar6@gmail.com>
2024-10-16 17:37:05 +0500

    first ever commits

---
+++ assets/js/script.js
@@ -0,0 +1,47 @@
+
+
+    function showContent(contentId) {
+        const content = document.getElementById('content');
+
+        const contentMap = {
+            content1: `
+                <img class="img-fluid" src="assets/images/Illustration Container.svg" alt="">
+                <div class="card-heading">
+                    Future-oriented orientation
+                    <p class="card-text">
+                       Explore the perfect path for your business in the digital world. We'll create a custom plan that aligns with your objectives and addresses evolving technology and market demands.
+                    </p>
+                </div>
+            `,
+            content2: `
+                <img class="img-fluid" src="assets/images/Illustration Container.svg" alt="">
+                <div class="card-heading">
+                    Digital transformation strategy
+                    <p class="card-text">
+                       Explore the perfect path for your business in the digital world. We'll create a custom plan that aligns with your objectives and addresses evolving technology and market demands.
+                    </p>
+                </div>
+            `,
+            content3: `
+                <img class="img-fluid" src="assets/images/Illustration Container.svg" alt="">
+                <div class="card-heading">
+                    Digital transformation implementation
+                    <p class="card-text">
+                       Explore the perfect path for your business in the digital world. We'll create a custom plan that aligns with your objectives and addresses evolving technology and market demands.
+                    </p>
+                </div>
+            `,
+            content4: `
+                <img class="img-fluid" src="assets/images/Illustration Container.svg" alt="">
+                <div class="card-heading">
+                    Digital transformation support
+                    <p class="card-text">
+                        Explore the perfect path for your business in the digital world. We'll create a custom plan that aligns with your objectives and addresses evolving technology and market demands.
+                    </p>
+                </div>
+            `
+        };
+
+        // Update the innerHTML of the content div with the appropriate content
+        content.innerHTML = contentMap[contentId] || '<p>No content available.</p>';
+    }
